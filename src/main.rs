a97b71b543624e4516b08759046ee2de2c6585a0
onzecki <git@oon.nz>
2024-10-08 05:11:24 +0200

    feat(reqwest): add user agent

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use reqwest::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut user_agent: String =
+        format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    if cfg!(debug_assertions) {
+        user_agent.push_str(" by ");
+        user_agent.push_str(env!("CARGO_PKG_AUTHORS"));
+        user_agent.push_str(" (DEBUGGING)");
+    }
+
+    let client: Client = reqwest::Client::builder()
+        .user_agent(user_agent.as_str())
+        .build()
+        .unwrap();
+    println!("{user_agent}");
 }
