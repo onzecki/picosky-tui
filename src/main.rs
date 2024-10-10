9c75522899e0cbc925f6ee866821d80a9d4808eb
onzecki <git@oon.nz>
2024-10-10 23:25:05 +0200

    feat!: complete ratatui rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,168 @@
-use color_eyre::{eyre::Context, Result};
-use futures_util::{StreamExt, TryStreamExt};
+use color_eyre::Result;
 use ratatui::{
-    crossterm::event::{self, Event, KeyCode},
-    widgets::{List, ListDirection, ListItem, Paragraph},
-    DefaultTerminal, Frame,
+    buffer::Buffer,
+    crossterm::{
+        event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+        style::Color,
+    },
+    layout::{Constraint, Layout, Rect},
+    style::{Modifier, Style, Stylize},
+    symbols,
+    text::Line,
+    widgets::{
+        Block, Borders, HighlightSpacing, List, ListItem, ListState, Paragraph, StatefulWidget,
+        Widget,
+    },
+    DefaultTerminal,
 };
 use reqwest::*;
-use reqwest_websocket::RequestBuilderExt;
-use serde::{Deserialize, Serialize};
+use serde::*;
 use serde_json::Number;
-use std::io;
-use std::time::Duration;
+use std::{collections::VecDeque, fmt};
+
+const MESSAGES_SHOWN: usize = 20;
+const URL: &str = "https://pico.api.bsky.mom/posts";
+const WS_URL: &str = "wss://pico.api.bsky.mom/subscribe";
 #[derive(Serialize, Deserialize, Clone)]
 struct Post {
     did: String,
-    handle: Option<String>,
+    handle: String,
     indexedAt: Number,
     nickname: Option<String>,
     post: String,
     rkey: String,
+    cid: String,
 }
 
+impl fmt::Display for Post {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} {}: {}\n",
+            &self.nickname.to_owned().unwrap_or(String::new()),
+            &self.handle,
+            &self.post
+        )
+    }
+}
 #[derive(Serialize, Deserialize)]
 struct Content {
     cursor: Number,
-    posts: Vec<Post>,
+    posts: VecDeque<Post>,
 }
 
-const MESSAGES_SHOWN: u16 = 20;
+#[derive(Default)]
+struct App {
+    client: Client,
+    cursor: usize,
+    post_state: ListState,
+    posts: VecDeque<Post>,
+    should_exit: bool,
+}
+
+impl App {
+    async fn load() -> Self {
+        let client: Client = reqwest::Client::builder()
+            .user_agent(construct_user_agent().as_str())
+            .build()
+            .unwrap();
+        let posts = get_history(client.clone()).await.unwrap();
+        let cursor = 0;
+        Self {
+            client,
+            cursor,
+            post_state: ListState::default(),
+            posts,
+            should_exit: false,
+        }
+    }
+    async fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        while !self.should_exit {
+            terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
+            if let Event::Key(key) = event::read()? {
+                self.handle_key(key);
+            };
+        }
+        Ok(())
+    }
+    fn handle_key(&mut self, key: KeyEvent) {
+        if key.kind != KeyEventKind::Press {
+            return;
+        }
+        match key.code {
+            KeyCode::Char('q') | KeyCode::Esc => self.should_exit = true,
+            KeyCode::Char('j') | KeyCode::Down => self.select_next(),
+            KeyCode::Char('k') | KeyCode::Up => self.select_previous(),
+            _ => {}
+        }
+    }
+    fn select_next(&mut self) {
+        self.post_state.select_next();
+    }
+    fn select_previous(&mut self) {
+        self.post_state.select_previous();
+    }
+}
+
+impl Widget for &mut App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [header_area, chat_area, footer_area] = Layout::vertical([
+            Constraint::Length(2),
+            Constraint::Fill(1),
+            Constraint::Length(1),
+        ])
+        .areas(area);
+
+        App::render_header(header_area, buf);
+        App::render_footer(footer_area, buf);
+        self.render_chat(chat_area, buf);
+    }
+}
+
+impl App {
+    fn render_header(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Picosky-TUI")
+            .bold()
+            .centered()
+            .render(area, buf);
+    }
+
+    fn render_footer(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Made by onzecki")
+            .centered()
+            .render(area, buf);
+    }
+
+    fn render_chat(&mut self, area: Rect, buf: &mut Buffer) {
+        let block = Block::new()
+            .borders(Borders::TOP)
+            .border_set(symbols::border::EMPTY)
+            .border_style(Style::new().fg(Color::White.into()))
+            .bg(Color::Black);
+
+        // Iterate through all elements in the `items` and stylize them.
+        let items: Vec<ListItem> = self
+            .posts
+            .iter()
+            .enumerate()
+            .map(|(_, todo_item)| {
+                let color = Color::Black;
+                ListItem::from(todo_item.to_string()).bg(color)
+            })
+            .collect();
+
+        // Create a List from all list items and highlight the currently selected one
+        let list = List::new(items)
+            .block(block)
+            .highlight_style(Style::new().add_modifier(Modifier::BOLD))
+            .highlight_symbol(">")
+            .highlight_spacing(HighlightSpacing::Always);
+
+        // We need to disambiguate this trait method as both `Widget` and `StatefulWidget` share the
+        // same method name `render`.
+        StatefulWidget::render(list, area, buf, &mut self.post_state);
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -38,47 +173,9 @@ async fn main() -> Result<()> {
         .user_agent(construct_user_agent().as_str())
         .build()
         .unwrap();
-
-    let url = format!(
-        "https://pico.api.bsky.mom/posts?limit={}&cursor=0",
-        MESSAGES_SHOWN
-    );
-    let ws_url = "wss://pico.api.bsky.mom/subscribe";
-    let app_result = run(terminal, client, url).await?;
+    let app_result = App::load().await.run(terminal).await;
     ratatui::restore();
-
     Ok(())
-}
-
-async fn run(mut terminal: DefaultTerminal, client: Client, history_url: String) -> Result<()> {
-    let list_items = get_history(client, history_url).await?;
-
-    loop {
-        terminal.draw(|frame| draw(frame, list_items.clone()))?; // Draw the list of posts
-        if should_quit()? {
-            break;
-        }
-    }
-    Ok(())
-}
-
-fn draw(frame: &mut Frame, list_items: Vec<ListItem>) {
-    let list = List::new(list_items).block(
-        ratatui::widgets::Block::default()
-            .title("Posts")
-            .borders(ratatui::widgets::Borders::ALL),
-    ).direction(ListDirection::BottomToTop);
-
-    frame.render_widget(list, frame.area());
-}
-
-fn should_quit() -> Result<bool> {
-    if event::poll(Duration::from_millis(250)).context("event poll failed")? {
-        if let Event::Key(key) = event::read().context("event read failed")? {
-            return Ok(KeyCode::Char('q') == key.code);
-        }
-    }
-    Ok(false)
 }
 
 fn construct_user_agent() -> String {
@@ -92,44 +189,14 @@ fn construct_user_agent() -> String {
     return user_agent;
 }
 
-fn post_display(post: Post) -> String {
-    format!(
-        "{} {}: {}\n",
-        post.nickname.to_owned().unwrap_or(String::new()),
-        post.handle.unwrap_or(String::new()),
-        post.post
-    )
-}
-
-async fn get_history(client: Client, history_url: String) -> Result<Vec<ListItem<'static>>> {
-    let initial_content: Content = client.get(history_url).send().await?.json().await?;
-    let list_items = initial_content
-        .posts
-        .into_iter()
-        .map(|post| {
-            let content = post_display(post);
-            ListItem::new(content)
-        })
-        .collect();
-
-    Ok(list_items)
-}
-
-async fn get_new_messages(client: Client, websocket_url: &str) -> Option<ListItem>{
-    let upgrade_response = client
-        .get(websocket_url)
-        .upgrade()
+async fn get_history(client: Client) -> Result<VecDeque<Post>> {
+    let initial_content: Content = client
+        .get(format!("{}?limit={}&cursor=0", URL, MESSAGES_SHOWN))
         .send()
-        .await
-        .unwrap();
-
-    let websocket = upgrade_response.into_websocket().await.unwrap();
-    let (mut _sender, mut receiver) = websocket.split();
-    if let Some(item) = receiver.try_next().await.unwrap() {
-        if let reqwest_websocket::Message::Text(json_post) = item {
-            let post: Post = serde_json::from_str(json_post.as_str()).unwrap();
-            return Some(ListItem::new(post_display(post)));
-        }
-    }
-    return None;
+        .await?
+        .json()
+        .await?;
+    let mut list_items: VecDeque<Post> = initial_content.posts;
+    list_items.make_contiguous().reverse();
+    Ok(list_items)
 }
